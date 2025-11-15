2c208d18b8968845fbb37f10928e5676d20705af
Athulya A.R <athulyaarkrishna@gmail.com>
2025-11-15 09:51:16 +0530

    adding routes

--- routes/habitRoutes.py
+++ routes/habitRoutes.py
@@ -12,6 +12,11 @@ habit_bp.route('', methods=['GET'])(get_user_habits)
 habit_bp.route('/today', methods=['GET'])(get_today_habits)
 habit_bp.route('/mark-done', methods=['POST'])(mark_habit_done)
 habit_bp.route('', methods=['DELETE'])(delete_habit)
+# Add these to existing routes
+habit_bp.route('/completed-today', methods=['GET'])(get_completed_today_habits)
+habit_bp.route('/failed-today', methods=['GET'])(get_failed_today_habits)
+habit_bp.route('/by-category', methods=['GET'])(get_habits_by_category)
+habit_bp.route('/daily-success', methods=['GET'])(get_daily_success_data)
 
 # Analytics routes
 habit_bp.route('/analytics', methods=['GET'])(get_user_analytics)
